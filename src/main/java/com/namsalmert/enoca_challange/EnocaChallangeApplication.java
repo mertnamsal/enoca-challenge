3a6e8eaa7766d7d3f3c6361f6c001309de3a6dbf
mertnamsal <namsalmert@gmail.com>
2023-07-29 12:02:16 +0300

    first commit

---
+++ src/main/java/com/namsalmert/enoca_challange/EnocaChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.namsalmert.enoca_challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocaChallangeApplication {
+
+    public static void main(String[] args) {
+       SpringApplication.run(EnocaChallangeApplication.class);
+    }
+
+}
---
+++ src/test/java/com/namsalmert/enoca_challange/EnocaChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namsalmert.enoca_challange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
